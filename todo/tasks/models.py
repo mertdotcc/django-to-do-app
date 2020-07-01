b7b181b8324b110f9c391b540383a4066a995e0a
Mert Öztürk <mert1ozturk@gmail.com>
2020-07-01 16:21:08 +0300

    forms

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
-    complete = models.BooleanField(default=False)
+    completed = models.BooleanField(default=False)
     creation_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import *
+from .forms import *
 
 
 def index(request):
-    return render(request, 'tasks/list.html')
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks':tasks , 'form':form}
+
+    return render(request, 'tasks/list.html', context)
